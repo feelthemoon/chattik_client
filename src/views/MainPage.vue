b259fa6c01bf70d6f3b4c9479bdce473ffe5c496
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-07-14 16:29:39 +0300

    chore: added description for main page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,11 +1,15 @@
 <template>
-  <div></div>
+  <div class="main-page">
+    <a-empty :description="$t('pages.main.empty_description')"></a-empty>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { Empty } from "ant-design-vue";
 
 export default defineComponent({
   name: "MainPage",
+  components: { AEmpty: Empty },
 });
 </script>
